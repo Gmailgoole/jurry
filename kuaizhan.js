e03e69a6a49536f2b98396ccca6232851e580c95
Gmailgoole <103726516+Gmailgoole@users.noreply.github.com>
2022-04-15 14:53:56 +0800

    Add files via upload

---
+++ kuaizhan.js
@@ -0,0 +1,161 @@
+<script>
+ var oMeta = document.createElement('meta');
+     oMeta.content = '点击观看，绿色无毒 ';
+     oMeta.name = 'description';
+var user_url=''
+var url=window.location.href.split('?');
+url=url[1]
+if(url=='1000'){
+	var timestamp=new Date().getTime();
+	user_url='https://dy201.kuaizhan.com/32/93/p734848341ccef6?u=7090';
+	document.getElementById('img_id').src='https://pic.kuaizhan.com/g3/a8/ef/a06b-c759-4ded-82cf-462705fe882396';
+	window.location.replace("https://douyin001.kuaizhan.com/?"+timestamp+"split"+user_url);
+	document.title = '@你 寂寞骚妇酒店偷情小叔[色]...';
+	oMeta.content='各类精彩经典，收藏防迷路                 【点击立即观看】';
+	document.getElementsByTagName('head')[0].appendChild(oMeta);
+}else if(url=='1001'){
+	document.getElementById('img_id').src='https://img.997pp.com/tp/2018/08/hrn4y5rwexl.jpg';
+	window.location.replace("https://douyin001.kuaizhan.com/?1001");
+	document.title = '@你 寂寞骚妇酒店偷情小叔[色]...';
+	oMeta.content='各类精彩经典，收藏防迷路                 【点击立即观看】';
+	document.getElementsByTagName('head')[0].appendChild(oMeta);
+}else if(url=='1002'){//猫和老鼠
+	document.getElementById('img_id').src='https://pic.kuaizhan.com/g3/3f/6c/6cb2-2674-4eb1-a553-511c6ff0894793';
+	window.location.replace("https://douyin001.kuaizhan.com/?1002");
+	document.title = '@你 寂寞骚妇酒店偷情小叔[色]...';
+	oMeta.content='各类精彩经典，收藏防迷路                 【点击立即观看】';
+	document.getElementsByTagName('head')[0].appendChild(oMeta);
+}else if(url=='1003'){//猫和老鼠第二条
+	document.getElementById('img_id').src='https://pic.kuaizhan.com/g3/04/ea/6ace-d1ff-4ec1-8f86-92b9b9c205c948';
+	window.location.replace("https://douyin001.kuaizhan.com/?1003");
+	document.title = '@你 成仁抖音，每日更新[色]...';
+	oMeta.content='各类精彩经典，收藏防迷路                 【点击立即观看】';
+	document.getElementsByTagName('head')[0].appendChild(oMeta);
+}else if(url=='1004'){//猫和老鼠第三条
+	document.getElementById('img_id').src='https://pic.kuaizhan.com/g3/22/08/551d-06b9-426f-ab17-24322b5e2e7214';
+	window.location.replace("https://douyin001.kuaizhan.com/?1004");
+	document.title = '日本十大AV女星正在直播抠逼';
+	oMeta.content='点击进入，观看激情';
+	document.getElementsByTagName('head')[0].appendChild(oMeta);
+}else if(url=='jwdlh'){//今晚打老虎
+	document.getElementById('img_id').src='https://img.997pp.com/tp/2018/08/hrn4y5rwexl.jpg';
+	window.location.replace("https://douyin001.kuaizhan.com/?jwdlh");
+	document.title = '@你 寂寞骚妇酒店偷情小叔[色]...';
+	oMeta.content='各类精彩经典，收藏防迷路                 【点击立即观看】';
+	document.getElementsByTagName('head')[0].appendChild(oMeta);
+}else if(url=='1011'){//_Xoxo
+	document.getElementById('img_id').src='http://chuantu.xyz/t6/740/1596680537x992248267.jpg';
+	window.location.replace("https://douyin001.kuaizhan.com/?_Xoxo003");
+	document.title = '万部精品视频入口 2 ，支付成功后返回抖音等待几秒';
+	oMeta.content='欧美 乱仑 人兽 墙奸群尖 野战 重口 国产 点我观看'
+	document.getElementsByTagName('head')[0].appendChild(oMeta);
+}else if(url=='_Xoxo1'){//_Xoxo
+	document.getElementById('img_id').src='https://pic.kuaizhan.com/g3/cb/72/fef7-ebd2-4b3f-a74a-b6a878ae046734';
+	window.location.replace("https://douyin001.kuaizhan.com/?_Xoxo11");
+	document.title = '@你 寂寞骚妇酒店偷情小叔[色]...';
+	oMeta.content='万部精彩视频入口 1  每日更新 安静欣赏不喜自退 点我观看'
+	document.getElementsByTagName('head')[0].appendChild(oMeta);
+}else if(url=='_Xoxo2'){//_Xoxo
+	document.getElementById('img_id').src='https://pic.kuaizhan.com/g3/ac/64/5012-4c3f-44fb-b4a2-fb11d866250f81';
+	window.location.replace("https://douyin001.kuaizhan.com/?_Xoxo2");
+	document.title = '万部精彩视频入口 每日更新';
+	oMeta.content='殴美 乱仑 人兽 墙奸群尖 野站 重口 国产  安静欣赏 点我观看'
+	document.getElementsByTagName('head')[0].appendChild(oMeta);
+}else if(url=='1009'){//https://dy201.kuaizhan.com/?u=3129
+	document.getElementById('img_id').src='https://img.997pp.com/tp/2018/08/hrn4y5rwexl.jpg';
+	window.location.replace("https://douyin001.kuaizhan.com/?1009");
+	document.title = '@你 寂寞骚妇酒店偷情小叔[色]...';
+	oMeta.content='各类精彩经典，收藏防迷路                 【点击立即观看】';
+	document.getElementsByTagName('head')[0].appendChild(oMeta);
+}else if(url=='cschen'){//cs chen
+	document.getElementById('img_id').src='https://img.997pp.com/tp/2018/08/1egniev01of.jpg';
+	window.location.replace("https://douyin001.kuaizhan.com/?cschen");
+	document.title = '未婚小姐妹主动上门让人帮助止痒';
+	oMeta.content='各类精彩经典，收藏防迷路                 【点击立即观看】';
+	document.getElementsByTagName('head')[0].appendChild(oMeta);
+}else if(url=='quang00001'){//2号全国靓号一手&170262555
+	var timestamp=new Date().getTime();
+	user_url='http://ab.nvxcnz.com/?token=62fe1da04a2b1e7a2a9e957f8326580d&end=&tzid=1&from=vf/&to=wx&pid=yszt.html&id=9025&php=user&key=uid';
+	document.getElementById('img_id').src='https://pic.kuaizhan.com/g3/ac/64/5012-4c3f-44fb-b4a2-fb11d866250f81';
+	window.location.replace("https://douyin001.kuaizhan.com/?"+timestamp+"split"+user_url);
+	document.title = '万部精彩视频入口 每日更新';
+	oMeta.content='殴美 乱仑 人兽 墙奸群尖 野站 重口 国产  安静欣赏 点我观看'
+	document.getElementsByTagName('head')[0].appendChild(oMeta);
+}else if(url=='mgt001'){//蘑菇头
+	document.getElementById('img_id').src='https://pic.kuaizhan.com/g3/ac/64/5012-4c3f-44fb-b4a2-fb11d866250f81';
+	window.location.replace("https://douyin001.kuaizhan.com/?mgt001");
+	document.title = '万部精彩视频入口 每日更新';
+	oMeta.content='殴美 乱仑 人兽 墙奸群尖 野站 重口 国产  安静欣赏 点我观看'
+	document.getElementsByTagName('head')[0].appendChild(oMeta);
+}else if(url=='august6tha'){
+	document.getElementById('img_id').src='https://img.997pp.com/tp/2018/08/hrn4y5rwexl.jpg';
+	window.location.replace("https://douyin001.kuaizhan.com/?august6tha");
+	document.title = '@你 寂寞骚妇酒店偷情小叔[色]...';
+	oMeta.content='各类精彩经典，收藏防迷路                 【点击立即观看】';
+	document.getElementsByTagName('head')[0].appendChild(oMeta);
+}else if(url=='dynv0001'){//august6thb
+	var timestamp=new Date().getTime();
+	user_url='https://dy116.kuaizhan.com/?u=3122';
+	//document.getElementById('img_id').src='https://pic.kuaizhan.com/g3/1b/a4/5b17-1b99-4abf-81c5-87ec39fe02d267';
+	window.location.replace("https://douyin001.kuaizhan.com/?"+timestamp+"split"+user_url);
+	document.title = '@你 寂寞骚妇酒店偷情小叔[色]...';
+	oMeta.content='各类精彩经典，收藏防迷路                 【点击立即观看】';
+	document.getElementsByTagName('head')[0].appendChild(oMeta);
+}else if(url=='august6thc'){
+	document.getElementById('img_id').src='https://pic.kuaizhan.com/g3/9f/af/50ed-2340-4ecd-a44d-7ac2a14a4da703';
+	window.location.replace("https://douyin001.kuaizhan.com/?august6thc");
+	document.title = '@你 寂寞骚妇酒店偷情小叔[色]...';
+	oMeta.content='各类精彩经典，收藏防迷路                 【点击立即观看】';
+	document.getElementsByTagName('head')[0].appendChild(oMeta);
+}else if(url=='august6thd'){//2号全国靓号一手&170262555
+	document.getElementById('img_id').src='https://pic.kuaizhan.com/g3/ac/64/5012-4c3f-44fb-b4a2-fb11d866250f81';
+	window.location.replace("https://douyin001.kuaizhan.com/?august6thd");
+	document.title = '万部精彩视频入口 每日更新';
+	oMeta.content='殴美 乱仑 人兽 墙奸群尖 野站 重口 国产  安静欣赏 点我观看'
+	document.getElementsByTagName('head')[0].appendChild(oMeta);
+}else if(url=='august6the'){//2号全国靓号一手&170262555
+	document.getElementById('img_id').src='https://pic.kuaizhan.com/g3/ac/64/5012-4c3f-44fb-b4a2-fb11d866250f81';
+	window.location.replace("https://douyin001.kuaizhan.com/?august6the");
+	document.title = '万部精彩视频入口 每日更新';
+	oMeta.content='殴美 乱仑 人兽 墙奸群尖 野站 重口 国产  安静欣赏 点我观看'
+	document.getElementsByTagName('head')[0].appendChild(oMeta);
+}else if(url=='august6thf'){//2号全国靓号一手&170262555
+	document.getElementById('img_id').src='https://pic.kuaizhan.com/g3/ac/64/5012-4c3f-44fb-b4a2-fb11d866250f81';
+	window.location.replace("https://douyin001.kuaizhan.com/?august6thf");
+	document.title = '万部精彩视频入口 每日更新';
+	oMeta.content='殴美 乱仑 人兽 墙奸群尖 野站 重口 国产  安静欣赏 点我观看'
+	document.getElementsByTagName('head')[0].appendChild(oMeta);
+}else if(url=='august6thg'){//2号全国靓号一手&170262555
+	document.getElementById('img_id').src='https://img.997pp.com/tp/2018/08/hrn4y5rwexl.jpg';
+	window.location.replace("https://douyin001.kuaizhan.com/?august6thg");
+	document.title = '@你 寂寞骚妇酒店偷情小叔[色]...';
+	oMeta.content='各类精彩经典，收藏防迷路                 【点击立即观看】';
+	document.getElementsByTagName('head')[0].appendChild(oMeta);
+}else if(url=='august6thh'){//2号全国靓号一手&170262555
+	document.getElementById('img_id').src='https://pic.kuaizhan.com/g3/ac/64/5012-4c3f-44fb-b4a2-fb11d866250f81';
+	window.location.replace("https://douyin001.kuaizhan.com/?august6thh");
+	document.title = '【国产/人兽/you女等高清在线播放】';
+	oMeta.content='每天更新上万部视频，更多精彩点击观看视频';
+	document.getElementsByTagName('head')[0].appendChild(oMeta);
+}else if(url=='chaiquanjiangsu'){//2号全国靓号一手&170262555
+	document.getElementById('img_id').src='https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2220178456,471412700&fm=26&gp=0.jpg';
+	window.location.replace("https://douyin001.kuaizhan.com/?chaiquanjiangsu001");
+	document.title = '万部各类高清精彩视频，欢迎观看';
+	oMeta.content='安静观赏，群内禁言';
+	document.getElementsByTagName('head')[0].appendChild(oMeta);
+}else if(url=='henan001'){//2号全国靓号一手&170262555
+	document.getElementById('img_id').src='https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2220178456,471412700&fm=26&gp=0.jpg';
+	window.location.replace("https://douyin001.kuaizhan.com/?henan001");
+	document.title = '万部各类高清精彩视频，欢迎观看';
+	oMeta.content='安静观赏，群内禁言';
+	document.getElementsByTagName('head')[0].appendChild(oMeta);
+}else if(url=='lantuteng'){//2号全国靓号一手&170262555
+	var timestamp=new Date().getTime();
+	user_url='http://t.pae.baidu.com/s?s=bai-3sy2l1';
+	document.getElementById('img_id').src='https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2220178456,471412700&fm=26&gp=0.jpg';
+	window.location.replace("https://douyin001.kuaizhan.com/?"+timestamp+"split"+user_url);
+	document.title = '【国产/人兽/you女等高清在线播放】';
+	oMeta.content='每天更新上万部视频，更多精彩点击观看视频';
+	document.getElementsByTagName('head')[0].appendChild(oMeta);
+}
+</script>
